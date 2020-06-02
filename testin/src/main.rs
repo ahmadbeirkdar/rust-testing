09fd9159bd32e54853afb863e24b1bbc98caf1b0
Ahmad Beirkdar <a@ahmad.ltd>
2020-06-01 22:31:09 -0400

    s

--- testin/src/main.rs
+++ testin/src/main.rs
@@ -6,36 +6,47 @@ fn main() {
     //     println!("{}",a[i]);
     // }
 
-    let s1 = String::from("hello");
-    let mut s2 = s1.clone();
-    s2.push_str("a");
+    // let s1 = String::from("hello");
+    // let mut s2 = s1.clone();
+    // s2.push_str("a");
 
+    // println!("{} {}", s1, s2);
+    // let s = String::from("hello");  // s comes into scope
 
-
-    println!("{} {}", s1, s2);
-    let s = String::from("hello");  // s comes into scope
-
-    takes_ownership(&s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
-                                    // NOW VALID WITH REF
+    // takes_ownership(&s);             // s's value moves into the function...
+    //                                 // ... and so is no longer valid here
+    //                                 // NOW VALID WITH REF
     
     
-    println!("{}", s);
+    // println!("{}", s);
 
-    let x = 5;                      // x comes into scope
+    // let x = 5;                      // x comes into scope
 
-    makes_copy(x);                  // x would move into the function,
-                                    // but i32 is Copy, so it’s okay to still
-                                    // use x afterward
+    // makes_copy(x);                  // x would move into the function,
+    //                                 // but i32 is Copy, so it’s okay to still
+    //                                 // use x afterward
 
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
 
-fn takes_ownership(some_string: &String) { // some_string comes into scope
-    println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+    // let mut a = "TESTT";
+    // for(i,j) in a.chars().enumerate(){
+       
+    // }
+    // println!("{}", a);
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
+    let mut a = "TESTT";
+    for i in 0..a.len(){
+       print!("{}",i);
+       a.replace(i, "a");
+    }
+    println!("{}", a);
+
+} 
+
+// fn takes_ownership(some_string: &String) { // some_string comes into scope
+//     println!("{}", some_string);
+// } // Here, some_string goes out of scope and `drop` is called. The backing
+//   // memory is freed.
+
+// fn makes_copy(some_integer: i32) { // some_integer comes into scope
+//     println!("{}", some_integer);
+// } // Here, some_integer goes out of scope. Nothing special happens.
